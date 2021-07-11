325221e58f6a38e4ec56886b62ce933cc26fb305
Éricles Dos Santos Cunha <67772327+Ericles-Porty@users.noreply.github.com>
2021-07-10 23:24:44 -0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,41 +1,19 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-
 public class App {
-    //Meus queridos #define ai embaixo
-    public static int REGIAO = 0;
-    public static int ESTADO = 1;
-    public static int MUNICIPIO = 2;
-    public static int CODUF = 3;
-    public static int CODMUN = 4;
-    public static int CODREGIAOSAUDE = 5;
-    public static int NOMEREGIAOSAUDE = 6;
-    public static int DATA = 7;
-    public static int SEMANAEPI = 8;
-    public static int POPULACAOTCU2019 = 9;
-    public static int CASOSACUMULADO = 10;
-    public static int CASOSNOVOS = 11;
-    public static int OBITOSACUMULADO = 12;
-    public static int OBITOSNOVOS = 13;
-    public static int RECUPERADOSNOVOS = 14;
-    public static int EMACOMPANHAMENTONOVOS = 15;
-    public static int INTERIOR = 16;
-    public static String path1 = System.getProperty("user.dir") + "\\BigData\\HIST_PAINEL_COVIDBR_2021_Parte1_07jul2021.csv";
-    public static String path2 = System.getProperty("user.dir") + "\\BigData\\HIST_PAINEL_COVIDBR_2021_Parte2_07jul2021.csv";
-    public static String path3 = System.getProperty("user.dir") + "\\BigData\\HIST_PAINEL_COVIDBR_2020_Parte1_07jul2021.csv";
-    public static String path4 = System.getProperty("user.dir") + "\\BigData\\HIST_PAINEL_COVIDBR_2020_Parte2_07jul2021.csv";
+
+    public static String path1 = System.getProperty("user.dir") + "\\BigData\\HIST_PAINEL_COVIDBR_2020_Parte1_07jul2021.csv";
+    public static String path2 = System.getProperty("user.dir") + "\\BigData\\HIST_PAINEL_COVIDBR_2020_Parte2_07jul2021.csv";
+    public static String path3 = System.getProperty("user.dir") + "\\BigData\\HIST_PAINEL_COVIDBR_2021_Parte1_07jul2021.csv";
+    public static String path4 = System.getProperty("user.dir") + "\\BigData\\HIST_PAINEL_COVIDBR_2021_Parte2_07jul2021.csv";
     public static String split = ";";
 
-
     public static void main(String[] args) {
-        String line = "";
 
-        ArvoreBB arvorebb = new ArvoreBB();
-        ArvoreAVL arvoreavl = new ArvoreAVL();
 
-        try {
+
+        //ArvoreBB arvorebb = new ArvoreBB();
+        //ArvoreAVL arvoreavl = new ArvoreAVL();
+
+        /*try {
             //Leitor de arquivo
             BufferedReader br = new BufferedReader(new FileReader(path4));
             //Roda ate achar a ultima linha
@@ -60,5 +38,7 @@ public class App {
         }
         //Rodou o programa ate o final
         System.out.println("Funcionou!");
+
+*/
     }
 }
