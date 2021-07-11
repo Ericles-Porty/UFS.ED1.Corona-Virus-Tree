f08f86215253d7a4101734d1d1c11e870c6dc2a9
Éricles Dos Santos Cunha <67772327+Ericles-Porty@users.noreply.github.com>
2021-07-10 23:27:33 -0300

    Update Info.java

--- src/Info.java
+++ src/Info.java
@@ -1,17 +1,23 @@
 public class Info {
-    public Consulta codigoMunicipio;
-    public Consulta data;
-    public Consulta uf;
+    public String chaveMunData;
+    public String chaveUF;
 
-    public String casosNovosCidade;
-    public String casosAcumuladosCidade;
-    public String obitosNovosCidade;
-    public String obitosAcumaladosCidade;
-    public String casosTotaisCidade;
-    public String obitosTotaisCidade;
-    public String casosTotaisRegiao;
-    public String obitosTotaisRegiao;
-    public String casosTotaisBrasil;
-    public String obitosTotaisBrasil;
+    public String regiao;
+    public String estado;
+    public String municipio;
+    public String coduf;
+    public String codmun;
+    public String codRegiaoSaude;
+    public String nomeRegiaoSaude;
+    public String data;
+    public String semanaEpi;
+    public String populacaoTcu2019;
+    public String casoAcumulado;
+    public String casosNovos;
+    public String obitosAcumulado;
+    public String obitosNovos;
+    public String recuperaDoNovos;
+    public String emcompanhamemtoNovos;
+    public String interior;
 
 }
